1c3fc3ccb33b65dfce8c927cdb2684461a6d97bb
nichaydel <nhaydel@ycharts.com>
2024-03-19 16:09:23 -0500

    use nvchad

---
+++ nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,35 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = {
+  html = {},
+  awk_ls = {},
+  bashls = {},
+  tsserver = {},
+
+  pyright = {
+    settings = {
+
+      python = {
+        analysis = {
+          autoImportCompletion = true,
+          useLibraryCodeForTypes = true,
+          autoSearchPaths = true,
+          typeCheckingMode = "off",
+        },
+      },
+    },
+  },
+}
+
+for name, opts in pairs(servers) do
+  opts.on_init = on_init
+  opts.on_attach = on_attach
+  opts.capabilities = capabilities
+
+  lspconfig[name].setup(opts)
+end
+
