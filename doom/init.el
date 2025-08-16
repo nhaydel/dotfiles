da7b5a621fb3e4b57c5f2e71700289731f6a6d0b
Nic Haydel <nhaydel@ycharts.com>
2025-08-16 12:38:52 -0500

    more emacs stuff

--- doom/init.el
+++ doom/init.el
@@ -115,8 +115,8 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)         ; C > C++ == 1
-       (clojure +lsp)           ; java with a lisp
+       (cc +lsp +rainbow-delimiters)         ; C > C++ == 1
+       (clojure +lsp +rainbow-delimiters)           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -135,7 +135,7 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       (go +lsp)         ; the hipster dialect
+       (go +lsp +rainbow-delimiters)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
