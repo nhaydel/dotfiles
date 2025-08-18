95ca71a7f44925f9a23854faaf67a94d00a80d04
nhaydel <nicholas.haydel1@gmail.com>
2025-08-17 22:04:59 -0500

    clojure support in emacs

--- doom/custom.el
+++ doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17" default)))
+   '("fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17" default))
+ '(package-selected-packages '(clojure-mode company a)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
